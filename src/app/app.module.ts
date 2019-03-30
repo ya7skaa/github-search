72cc26a1480733fa7247d57b5f88e30dbf81c80f
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-03-30 11:43:01 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchFormComponent,
+    UserProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
